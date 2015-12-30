74887c5100a31714f6f93d4e84f6633ff5a4d883
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2015-12-30 11:45:45 -0200

    Adding gserver script.

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 from distutils.core import setup
 
 setup(name="libdict",
-      version="0.1",
+      version="0.2",
       py_modules=['libdict'],
-      scripts=['gdict'],
+      scripts=['gdict', 'gserver'],
       description = 'A python lib to use google translator.',
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br")
+
+
 
 
 
